21ba0b286357ed627d95dad342126ee62f23d0ee
jensenbrown19 <jensen.brown@smu.ca>
2021-11-26 17:16:11 -0400

    Joined tables to create one complete table

--- finalproject.R
+++ finalproject.R
@@ -7,9 +7,8 @@ library(readxl)
 library(tidyverse)
 
 class_data <- read_xlsx("ENVS 4826 Project Data.xlsx")
-excel_sheets("ENVS 4826 Project Data.xlsx")
-project_data <- excel_sheets("ENVS 4826 Project Data.xlsx") %>% map_df(~read_xlsx("ENVS 4826 Project Data.xlsx", .))
-
+tree_data <-  read_xlsx("SMUtreedatabase2021.xlsx")
+project_data <- merge(class_data, tree_data, by = "uid_4826")
 
 campus_buildings <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "CampusBuildings")
 greenspace <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Greenspace")
