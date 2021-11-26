5e7e08b47419c44e991603fd0530d2f2b2d691d0
jensenbrown19 <jensen.brown@smu.ca>
2021-11-26 17:02:49 -0400

    Merged spreadsheets into one table

--- finalproject.R
+++ finalproject.R
@@ -2,11 +2,17 @@
 # Jensen Brown & Cameron Zinn
 
 library(rgdal)
+library(ggplot2)
+library(readxl)
+library(tidyverse)
 
 class_data <- read_xlsx("ENVS 4826 Project Data.xlsx")
-tree_data <- read.csv("SMUtreedatabase2021.csv")
+excel_sheets("ENVS 4826 Project Data.xlsx")
+project_data <- excel_sheets("ENVS 4826 Project Data.xlsx") %>% map_df(~read_xlsx("ENVS 4826 Project Data.xlsx", .))
+
 
 campus_buildings <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "CampusBuildings")
 greenspace <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Greenspace")
 pathways <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Pathways")
 pavement <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Pavement")
+
