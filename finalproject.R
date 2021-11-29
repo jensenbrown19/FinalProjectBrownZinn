c99a4afb56baa53edd1d6ed4803a9802f3d85970
jensenbrown19 <jensen.brown@smu.ca>
2021-11-29 15:42:28 -0400

    did a bunch of sh*t

--- finalproject.R
+++ finalproject.R
@@ -11,9 +11,14 @@ library(dplyr)
 library(sf)
 library(sp)
 library(spdep)
+library(foreign)
 
 # Reading spreadsheets, merging spreadsheets, and filtering spreadsheet
-class_data <- read_xlsx("ENVS 4826 Project Data.xlsx")
+class_data <- st_read("ENVS 4826 Project Data.csv",
+                      options = c("X_POSSIBLE_NAMES=x", 
+                                  "Y_POSSIBLE_NAMES=y"),
+                      crs = 4326)
+st_crs(class_data)
 tree_data <-  read_xlsx("SMUtreedatabase2021.xlsx")
 project_data <- merge(class_data, tree_data, by = "uid_4826")
 campus_data <- filter(project_data, location == "SMU campus")
@@ -26,6 +31,7 @@ campus_buildings <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Scien
 greenspace <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Greenspace")
 pathways <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Pathways")
 pavement <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Pavement")
+campus_data_fixed <- st_write(campus_data, "campus_data_fixed.shp")
 
 # Fortifying shapefiles to be dataframes
 campus_buildings_df <- fortify(campus_buildings)
@@ -39,7 +45,7 @@ ggplot() +
   geom_polygon(data = pavement_df, aes(x = long, y = lat, group = group), colour = "Black", fill = "Gray") +
   geom_point(data = campus_data, aes(x = UTMX, y = UTMY, col = crown_condition_int), size = 1) +
   scale_colour_manual(values = c('Red', 'Green', 'Black'), labels = c('Fair/Poor', 'Good', 'NA')) +
-  labs(title = "Crown Condition of Trees Around SMU Campus with Respect to Infrastructure", x = "UTMX", y = "UTMY", colour = "Crown Condition") +
+  labs(title = "Crown Condition of Trees Around SMU Campus", x = "UTMX", y = "UTMY", colour = "Crown Condition") +
   theme_bw()
 
 # Plotting Trunk Damage Map
@@ -48,18 +54,15 @@ ggplot() +
   geom_polygon(data = pavement_df, aes(x = long, y = lat, group = group), colour = "Black", fill = "Gray") +
   geom_point(data = campus_data, aes(x = UTMX, y = UTMY, col = trunk_damage), size = 1) +
   scale_colour_manual(values = c('Green', 'Red', 'Black'), labels = c('Absent', 'Present', 'NA')) +
-  labs(title = "Presence of Trunk Damage Trees Around SMU Campus with Respect to Infrastructure", x = "UTMX", y = "UTMY", colour = "Crown Condition") +
+  labs(title = "Presence of Trunk Damage Trees Around SMU Campus", x = "UTMX", y = "UTMY", colour = "Crown Condition") +
   theme_bw()
 
-# Creating distance function
-calculate_distance_utm <- function(x1, x2, y1, y2) {
-  distance = sqrt((x2 - x1)^2 + (y2 - y1)^2)
-  return(distance)
-}
+campus_data_fixed <- st_read("C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/campus_data_fixed.shp")
+st_crs(campus_data_fixed)
+st_crs(campus_buildings)
+campus_data_fixed <- st_set_crs(campus_data_fixed, 26920)
 
-# Calculating crown distance
-distance_crown <- calculate_distance_utm(campus_buildings_df$long, campus_data$UTMX, campus_buildings_df$lat, campus_data$UTMY)
-campus_data$distance_crown <- distance_crown
 
+tree_distances <- min(st_distance(campus_buildings, campus_data_fixed[1, ]))
 
 lm_crown <- lm(crown_condition_int ~ distance_crown, data = )
