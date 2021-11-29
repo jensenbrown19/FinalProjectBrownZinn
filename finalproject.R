50c39d7d71aae9d3d01b1bbc5933f431c55681ce
jensenbrown19 <jensen.brown@smu.ca>
2021-11-29 11:47:55 -0400

    Added interger column for crown condition

--- finalproject.R
+++ finalproject.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(readxl)
 library(tidyverse)
 library(rgeos)
+library(dplyr)
 
 # Reading spreadsheets, merging spreadsheets, and filtering spreadsheet
 class_data <- read_xlsx("ENVS 4826 Project Data.xlsx")
@@ -14,6 +15,8 @@ tree_data <-  read_xlsx("SMUtreedatabase2021.xlsx")
 project_data <- merge(class_data, tree_data, by = "uid_4826")
 campus_data <- filter(project_data, location == "SMU campus")
 campus_data %>% drop_na(crown_condition)
+crown_condition_int <- recode(campus_data$crown_condition, 'G' = 'G', 'F' = 'FP', 'P' = 'FP')
+campus_data$crown_condition_int <- crown_condition_int
 
 # Reading shapefiles
 campus_buildings <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "CampusBuildings")
@@ -30,6 +33,7 @@ pavement_df <- fortify(pavement)
 # Plotting Map
 ggplot() +
   geom_polygon(data = campus_buildings_df, aes(x = long, y = lat, group = group), colour = "Blue", fill = "Light Blue") +
+  geom_polygon(data = pavement_df, aes(x = long, y = lat, group = group), colour = "Black", fill = "Gray") +
   geom_point(data = campus_data, aes(x = UTMX, y = UTMY, col = crown_condition), size = 1) +
   scale_colour_manual(values = c('Yellow', 'Green', 'Red', 'Black'), labels = c('Fair', 'Good', 'Poor', 'NA')) +
   labs(title = "Crown Condition of Trees Around SMU Campus", x = "Longitude", y = "Latitude", colour = "Crown Condition") +
